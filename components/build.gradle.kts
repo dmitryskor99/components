46427a03938b1534be6092cdb8dba264ae749140
Dmitrii.Skorodumov <dmitrii_skor@MacBook-Pro-Dmitrij.local>
2024-04-21 14:55:13 +0300

    add compose and created first component - text

--- components/build.gradle.kts
+++ components/build.gradle.kts
@@ -40,4 +40,12 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation(platform("androidx.compose:compose-bom:2024.04.01"))
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    implementation("androidx.activity:activity-compose:1.9.0")
+    implementation("androidx.navigation:navigation-compose:2.7.7")
 }
