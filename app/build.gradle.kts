7377da094ca7fd723548804abd9467ac0526221e
Dmitrii.Skorodumov <dmitrii_skor@MacBook-Pro-Dmitrij.local>
2024-04-21 19:48:35 +0300

    release text next try

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,8 +40,10 @@ dependencies {
     implementation("androidx.core:core-ktx:1.13.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
-    implementation(project(":components"))
+//    implementation(project(":components"))
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation("com.github.dmitryskor99:components:1.0.1")
 }
